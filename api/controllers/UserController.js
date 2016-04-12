7f3eee4e47d38ac1dc108f086792352d22d5d215
Hunter Sadler <hsadlersemail@gmail.com>
2016-04-11 17:20:44 -0700

    Login / Registration Backend

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,7 +5,51 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+var bcrypt = require('bcrypt');
 
+module.exports = {
+	login: function (req, res) {
+		User.findOne({
+			userName: req.body.userName
+		},
+		function(err, user) {
+			if (err) {
+				res.json({ error: 'DB error' }, 500);
+			}
+			if (!user) {
+				res.view('home');
+			}
+			else {
+				bcrypt.compare(req.body.password, user.password, function(err, match){
+					if (match) {
+						//console.log('login');
+						req.session.name = user.name;
+						req.session.user = user.id;
+						res.redirect('/');
+					}
+					else {
+						res.view('error', {error: 'Password Not Correct'});
+					}
+				});
+			}
+		});
+	},
+
+	logout: function (req, res) {
+		//console.log(req.session);
+		req.session.destroy(function() {
+			res.redirect('/');
+		});
+	},
+
+	create: function(req, res) {
+		User.create(req.body).exec(function(err, user) {
+			if (err) {
+				return res.view('error', {error: 'Username/Email May Already Be Taken, Or Incorrect Password'});
+			}
+			req.session.name = user.name;
+			req.session.user = user.id;
+			return res.redirect('/');
+		});
+	}
+};
--- api/models/User.js
+++ api/models/User.js
@@ -5,13 +5,17 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
-module.exports = {
+var bcrypt = require('bcrypt');
 
+module.exports = {
   attributes: {
     fName: {
       type: 'string',
     },
     lName: {
+      type: 'string',
+    },
+    userName: {
       type: 'string',
     },
     password: {
@@ -39,5 +43,18 @@ module.exports = {
       collection: 'comment',
       via: 'author',
     }
-  }
+  },
+  beforeCreate: function (attrs, cb)
+  {
+    bcrypt.genSalt(10, function(err, salt)
+      {
+        if (err) return cb(err);
+        bcrypt.hash(attrs.password, salt, function(err, hash)
+          {
+            if (err) return cb(err);
+            attrs.password = hash;
+            cb();
+          });
+      });
+ }
 };
